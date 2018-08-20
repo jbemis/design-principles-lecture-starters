1187ba5c5763c0d9a83efd3b424835e982ef03a4
Joel Bemis <joel.bemis@gmail.com>
2018-08-20 13:40:58 +0300

    coursework
    implemented

--- RefactoredHairSalonStarter/src/model/HairSalon.java
+++ RefactoredHairSalonStarter/src/model/HairSalon.java
@@ -8,6 +8,8 @@ public class HairSalon {
 
     private ArrayList<Customer> bookings;
     private Map<Treatment, ArrayList<String>> treatmentSpecialists = new HashMap<>();
+    SpecialistSuggester specialistSuggester = new SpecialistSuggester();
+
 
     // EFFECTS: creates a hair salon with available booking times from 0-17hrs.
     public HairSalon(){
@@ -17,21 +19,21 @@ public class HairSalon {
             bookings.add(i, null);
         }
 
-        Treatment cut = new Treatment("cut", 1);
-        Treatment colour = new Treatment ("colour", 2);
-        Treatment condition = new Treatment ("condition", 3);
-
-        addTreatment(cut);
-        addTreatment(colour);
-        addTreatment(condition);
-
-        addTreatmentSpecialist(cut, "Jean");
-        addTreatmentSpecialist(cut, "Marie");
-        addTreatmentSpecialist(cut, "Gerome");
-        addTreatmentSpecialist(colour, "Sylvie");
-        addTreatmentSpecialist(colour, "Sandrine");
-        addTreatmentSpecialist(condition, "Mike");
-        addTreatmentSpecialist(condition, "Bob");
+//        Treatment cut = new Treatment("cut", 1);
+//        Treatment colour = new Treatment ("colour", 2);
+//        Treatment condition = new Treatment ("condition", 3);
+//
+//        addTreatment(cut);
+//        addTreatment(colour);
+//        addTreatment(condition);
+//
+//        addTreatmentSpecialist(cut, "Jean");
+//        addTreatmentSpecialist(cut, "Marie");
+//        addTreatmentSpecialist(cut, "Gerome");
+//        addTreatmentSpecialist(colour, "Sylvie");
+//        addTreatmentSpecialist(colour, "Sandrine");
+//        addTreatmentSpecialist(condition, "Mike");
+//        addTreatmentSpecialist(condition, "Bob");
     }
 
 
@@ -39,10 +41,7 @@ public class HairSalon {
     // EFFECTS: books the customer into the requested timeslot if it is a valid timeslot,
     //          and lets the Customer know the booking time.
     public boolean makeNewBooking(Customer c, int bookingTime, Treatment treatment){
-        if (bookingTime >= bookings.size() || bookingTime+treatment.getHoursNeeded()>bookings.size()){
-            System.out.println("We can't process that booking time");
-            return false;
-        }
+        if (checkBookingTimeValidity(bookingTime, treatment)) return false;
         for (int i = bookingTime; i < bookingTime+treatment.getHoursNeeded(); i++) {
             System.out.println("Customer "+c.getName()+" has been booked at "+bookingTime);
             bookings.set(i,c);
@@ -51,12 +50,17 @@ public class HairSalon {
         return true;
     }
 
-
-    public boolean cancelBooking(Customer c, int bookingTime, Treatment treatment){
+    private boolean checkBookingTimeValidity(int bookingTime, Treatment treatment) {
         if (bookingTime >= bookings.size() || bookingTime+treatment.getHoursNeeded()>bookings.size()){
             System.out.println("We can't process that booking time");
-            return false;
+            return true;
         }
+        return false;
+    }
+
+
+    public boolean cancelBooking(Customer c, int bookingTime, Treatment treatment){
+        if (checkBookingTimeValidity(bookingTime, treatment)) return false;
         for (int i = bookingTime; i < bookingTime+treatment.getHoursNeeded(); i++) {
             System.out.println("Time: "+bookingTime+" has been cancelled");
             bookings.set(i,null);
@@ -117,13 +121,13 @@ public class HairSalon {
     }
 
     public ArrayList<String> suggestConsultant(Treatment treatment) {
-        System.out.print("May we suggest you work with: ");
-        ArrayList<String> names = treatmentSpecialists.get(treatment);
-        for (String name : names){
-            System.out.print(name+"? ");
-        }
-        System.out.println();
-        return names;
+//        System.out.print("May we suggest you work with: ");
+////        ArrayList<String> names = treatmentSpecialists.get(treatment);
+////        for (String name : names){
+////            System.out.print(name+"? ");
+////        }
+////        System.out.println();
+        return specialistSuggester.suggestConsultant(treatment);
     }
 
     private void addTreatment(Treatment treatment){
